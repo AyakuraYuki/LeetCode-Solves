cc2ae041d4b5251e286acef3fd8fbcba70e4c13d
Ayakura Yuki <sakurajuzi@live.com>
2022-11-22 18:49:59 +0800

    + c# solutions

---
+++ CSharp/P242ValidAnagram/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace cc.ayakurayuki.leetcode.csharp.solution.P242ValidAnagram
+{
+    /// <summary>
+    /// 242. Valid Anagram
+    ///
+    /// https://leetcode.com/problems/valid-anagram/description/
+    /// </summary>
+    public class Solution
+    {
+        public bool IsAnagram(string s, string t)
+        {
+            s = stringSort(s);
+            t = stringSort(t);
+            return s.Equals(t);
+        }
+
+        public static string stringSort(string str)
+        {
+            char[] ch = new char[512];
+            ch = str.ToCharArray();
+            Array.Sort(ch);
+            return new string(ch);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(new Solution().IsAnagram("anagram", "nagaram")); // True
+            Console.WriteLine(new Solution().IsAnagram("rat", "car")); // False
+        }
+    }
+}
