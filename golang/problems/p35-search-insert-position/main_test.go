eb62dc47255659a60dda22ff84dfd82d4bdf94a1
Ayakura Yuki <YukiAyakura@outlook.com>
2024-06-16 04:00:10 +0800

    + solutions

---
+++ golang/problems/p35-search-insert-position/main_test.go
@@ -0,0 +1,21 @@
+package main
+
+import "testing"
+
+func Test_searchInsert(t *testing.T) {
+	cases := []struct {
+		Nums   []int
+		Target int
+		Want   int
+	}{
+		{[]int{1, 3, 5, 6}, 5, 2},
+		{[]int{1, 3, 5, 6}, 2, 1},
+		{[]int{1, 3, 5, 6}, 7, 4},
+	}
+	for _, v := range cases {
+		get := searchInsert(v.Nums, v.Target)
+		if get != v.Want {
+			t.Fatalf("want %d but get %d", v.Want, get)
+		}
+	}
+}
