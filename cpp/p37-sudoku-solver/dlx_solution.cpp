c9df232fc28a6d37657d7cfda58417abd701407a
Ayakura Yuki <YukiAyakura@outlook.com>
2024-06-26 15:46:06 +0800

    + p37 sudoku solver

---
+++ cpp/p37-sudoku-solver/dlx_solution.cpp
@@ -0,0 +1,177 @@
+/*
+舞蹈链算法
+ */
+
+#include <vector>
+
+using namespace std;
+
+const int N = 9 * 9 * 9 * 4;
+
+class node {
+public:
+    int u{}, d{}, l{}, r{}; // 上下左右
+    int row{}, col{}; // 具体坐标
+    int s{}; // col 节点数量
+    int h{}; // row 头节点
+    int ans{}; // 被选的 row
+    node() = default;
+};
+
+class DLX {
+public:
+    vector<vector<int> > a;
+    const int n = 9 * 9 * 9, m = 9 * 9 * 4;
+    int num{};
+    vector<node> nodes;
+
+    explicit DLX(vector<vector<int> > &B) {
+        a = B;
+        dance();
+        B = a;
+    }
+
+    void dance() {
+        init();
+        for (int i = 0; i < 9; ++i) {
+            for (int j = 0; j < 9; ++j) {
+                for (int k = 1; k <= 9; ++k) {
+                    if (a[i][j] != k && a[i][j] != 0) continue;
+                    const int id = i * 9 * 9 + j * 9 + k;
+                    Link(id, i * 9 + j + 1);
+                    Link(id, i * 9 + k + 81);
+                    Link(id, j * 9 + k + 162);
+                    Link(id, (i / 3 * 3 + j / 3) * 9 + k + 243);
+                }
+            }
+        }
+        Dance(0);
+    }
+
+    void init() {
+        num = m + 1;
+        nodes.assign(N, node());
+        for (int i = 0; i <= m; ++i) {
+            node &it = nodes[i];
+            it.l = i - 1; // 左
+            it.r = i + 1; // 右
+            it.u = it.d = i; // 上下
+        }
+        nodes[0].l = m;
+        nodes[m].r = 0;
+    }
+
+    void Link(int r, int c) {
+        ++num;
+        node &row = nodes[r], &col = nodes[c], &nums = nodes[num];
+
+        nums.row = r;
+        nums.col = c;
+        ++col.s;
+
+        // node[col.u] <-> nums <-> col
+        nums.u = col.u;
+        nodes[col.u].d = num;
+        col.u = num;
+        nums.d = c;
+
+        if (row.h == 0) row.h = nums.l = nums.r = num;
+        else {
+            node &head = nodes[row.h];
+            // node[head.l] <-> nums <-> head
+            nums.l = head.l;
+            nodes[head.l].r = num;
+            head.l = num;
+            nums.r = row.h;
+        }
+    }
+
+    void Remove(int c) {
+        const node &col = nodes[c];
+
+        // node[col.l] <-> node[col.r]
+        nodes[col.l].r = col.r;
+        nodes[col.r].l = col.l;
+
+        // 下右
+        for (int i = col.d; i != c; i = nodes[i].d) {
+            for (int j = nodes[i].r; j != i; j = nodes[j].r) {
+                const node &it = nodes[j];
+                //node[it.u] <-> node[it.d]
+                nodes[it.d].u = it.u;
+                nodes[it.u].d = it.d;
+                nodes[it.col].s--;
+            }
+        }
+    }
+
+    void Resume(int c) {
+        // node[col.l] -> node[c] <- node[col.r]
+        const node &col = nodes[c];
+        nodes[col.r].l = c;
+        nodes[col.l].r = c;
+
+        //上左
+        for (int i = nodes[c].u; i != c; i = nodes[i].u) {
+            for (int j = nodes[i].l; j != i; j = nodes[j].l) {
+                const node &it = nodes[j];
+                //node[it.u] -> node[j] <- node[it.d]
+                nodes[it.d].u = j;
+                nodes[it.u].d = j;
+                nodes[it.col].s++;
+            }
+        }
+    }
+
+    int Dance(int dep) {
+        if (nodes[0].r == 0) {
+            for (int i = 0; i < dep; ++i) {
+                const node &it = nodes[i];
+                const int x = (it.ans - 1) / 9 / 9;
+                const int y = (it.ans - 1) / 9 % 9;
+                const int val = (it.ans - 1) % 9 + 1;
+                a[x][y] = val;
+            }
+            return 1;
+        }
+
+        int c = nodes[0].r;
+        for (int i = nodes[0].r; i != 0; i = nodes[i].r) {
+            if (nodes[i].s == 0 || nodes[c].s == 0) return 0;
+            if (nodes[i].s < nodes[c].s) c = i;
+        }
+        Remove(c);
+
+        for (int i = nodes[c].d; i != c; i = nodes[i].d) {
+            node &it = nodes[i];
+            nodes[dep].ans = it.row;
+            for (int j = nodes[i].r; j != i; j = nodes[j].r) Remove(nodes[j].col);
+            if (Dance(dep + 1) == 1) return 1;
+            for (int j = nodes[i].l; j != i; j = nodes[j].l) Resume(nodes[j].col);
+        }
+        Resume(c);
+
+        return 0;
+    }
+};
+
+class DLXSolution {
+public:
+    void solveSudoku(vector<vector<char> > &board) {
+        // DLX处理数值，把谜题板从【字符板】转换成相应的【数字板】
+        vector a(9, vector<int>(9));
+        for (int i = 0; i < 9; ++i) {
+            for (int j = 0; j < 9; ++j) {
+                a[i][j] = board[i][j] == '.' ? 0 : board[i][j] - '0'; // 待填入格子设为0，其余设为对应数值
+            }
+        }
+        // DLX算法解决数独
+        DLX solver(a);
+        // 把结果板转换成字符板，赋给 board
+        for (int i = 0; i < 9; ++i) {
+            for (int j = 0; j < 9; ++j) {
+                board[i][j] = static_cast<char>(a[i][j] + '0');
+            }
+        }
+    }
+};
---
+++ cpp/p37-sudoku-solver/main.cpp
@@ -0,0 +1,89 @@
+//
+// Created by 绫仓优希 on 2024-06-26.
+//
+
+/*
+37. 解数独
+
+https://leetcode.cn/problems/sudoku-solver/
+
+编写一个程序，通过填充空格来解决数独问题。
+
+数独的解法需 遵循如下规则：
+1. 数字 1-9 在每一行只能出现一次。
+2. 数字 1-9 在每一列只能出现一次。
+3. 数字 1-9 在每一个以粗实线分隔的 3x3 宫内只能出现一次。（请参考示例图）
+
+数独部分空格内已填入了数字，空白格用 '.' 表示。
+
+示例 1：
+    （图示：puzzle-images/p37-sudoku-solves/input.png）
+    输入：board = [
+        ["5","3",".",".","7",".",".",".","."],
+        ["6",".",".","1","9","5",".",".","."],
+        [".","9","8",".",".",".",".","6","."],
+        ["8",".",".",".","6",".",".",".","3"],
+        ["4",".",".","8",".","3",".",".","1"],
+        ["7",".",".",".","2",".",".",".","6"],
+        [".","6",".",".",".",".","2","8","."],
+        [".",".",".","4","1","9",".",".","5"],
+        [".",".",".",".","8",".",".","7","9"]
+    ]
+    （图示：puzzle-images/p37-sudoku-solves/output.png）
+    输出：[
+        ["5","3","4","6","7","8","9","1","2"],
+        ["6","7","2","1","9","5","3","4","8"],
+        ["1","9","8","3","4","2","5","6","7"],
+        ["8","5","9","7","6","1","4","2","3"],
+        ["4","2","6","8","5","3","7","9","1"],
+        ["7","1","3","9","2","4","8","5","6"],
+        ["9","6","1","5","3","7","2","8","4"],
+        ["2","8","7","4","1","9","6","3","5"],
+        ["3","4","5","2","8","6","1","7","9"]
+    ]
+    解释：输入的数独如上图所示，唯一有效的解决方案如下所示：
+
+提示：
+  * board.length == 9
+  * board[i].length == 9
+  * board[i][j] 是一位数字或者 '.'
+  * 题目数据 保证 输入数独仅有一个解
+ */
+
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    void solveSudoku(vector<vector<char> > &board) {
+        dfs(board, 0, 0);
+    }
+
+private:
+    // 递归的回溯法
+    static bool dfs(vector<vector<char> > &board, int row, int col) {
+        if (col == board[0].size()) {
+            ++row;
+            col = 0;
+        }
+        if (row == board.size()) return true;
+        if (board[row][col] != '.') return dfs(board, row, col + 1);
+        for (char ch = '1'; ch <= '9'; ++ch) {
+            if (invalid(board, row, col, ch)) {
+                board[row][col] = ch;
+                if (dfs(board, row, col + 1)) return true;
+                board[row][col] = '.';
+            }
+        }
+        return false;
+    }
+
+    static bool invalid(const vector<vector<char> > &board, const int row, const int col, const char ch) {
+        for (int i = 0; i < 9; ++i) {
+            if (board[row][i] == ch || board[i][col] == ch || board[row / 3 * 3 + i / 3][col / 3 * 3 + i % 3] == ch)
+                return false;
+        }
+        return true;
+    }
+};
