59e39ff4a3ab622bb8e6dcd07474578e2e6ef7db
Ayakura Yuki <YukiAyakura@outlook.com>
2024-09-11 11:00:38 +0800

    @ p18-4sum: optimized

--- cpp/p18-4sum/solution.cpp
+++ cpp/p18-4sum/solution.cpp
@@ -72,3 +72,55 @@ public:
         return ans;
     }
 };
+
+class OptimizedSolution {
+public:
+    static vector<vector<int> > four_sum(vector<int> &nums, const int target) {
+        if (nums.size() < 4) return {};
+
+        vector<vector<int> > ans;
+        const size_t n = nums.size();
+        ranges::sort(nums);
+
+        size_t l = 0, r = 0;
+
+        for (size_t i = 0; i < n - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue; // 与前一个数相同，跳过
+
+            if (static_cast<long>(nums[i]) + nums[i + 1] + nums[i + 2] + nums[i + 3] > target) {
+                break; // 确定第一个数，再与剩下三个数求和，超过 target，则退出循环
+            }
+            if (static_cast<long>(nums[i]) + nums[n - 3] + nums[n - 2] + nums[n - 1] < target) {
+                continue; // 确定第一个数，再与数组末尾三个数求和，小于 target，则跳过当前 i 的数值，进入下一个循环
+            }
+
+            for (size_t j = i + 1; j < n - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue; // 与前一个数相同，跳过
+
+                if (static_cast<long>(nums[i]) + nums[j] + nums[j + 1] + nums[j + 2] > target) {
+                    break; // 确定第二个数，再与接下来的两个数和第一个数求和，超过 target，则退出循环
+                }
+                if (static_cast<long>(nums[i]) + nums[j] + nums[n - 2] + nums[n - 1] < target) {
+                    continue; // 确定第二个数，再与第一个数和数组末尾两个数求和，小于 target，则跳过当前 j 的数值，进入下一个循环
+                }
+
+                l = j + 1, r = n - 1;
+                while (l < r) {
+                    if (const long sum = static_cast<long>(nums[i]) + nums[j] + nums[l] + nums[r]; sum == target) {
+                        ans.push_back({nums[i], nums[j], nums[l], nums[r]});
+                        while (l < r && nums[l] == nums[l + 1]) ++l; // 跳过重复
+                        while (l < r && nums[r] == nums[r - 1]) --r; // 跳过重复
+                        ++l;
+                        --r;
+                    } else if (sum < target) {
+                        ++l;
+                    } else {
+                        --r;
+                    }
+                }
+            }
+        }
+
+        return ans;
+    }
+};
