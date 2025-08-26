85ad10bfedcafcf1b8a0e1d232ea801a24727a02
Ayakura Yuki <YukiAyakura@outlook.com>
2025-08-26 10:08:51 +0800

    @ practice

--- golang/practise/labyrinth/internal/solution/solution.go
+++ golang/practise/labyrinth/internal/solution/solution.go
@@ -13,7 +13,6 @@ type Point struct {
 func BFS(R, C int, labyrinth [50][50]rune) int {
 	var (
 		distance [50][50]int
-		visited  [50][50]bool
 		q        []Point
 	)
 	for r := 0; r < 50; r++ {
@@ -24,7 +23,6 @@ func BFS(R, C int, labyrinth [50][50]rune) int {
 
 	// 出生点
 	distance[1][1] = 1
-	visited[1][1] = true
 	q = append(q, Point{1, 1})
 
 	for len(q) > 0 {
@@ -33,8 +31,7 @@ func BFS(R, C int, labyrinth [50][50]rune) int {
 		x, y := f.x, f.y
 		for i := 0; i < 4; i++ {
 			tx, ty := x+dx[i], y+dy[i]
-			if tx >= 1 && tx <= R && ty >= 1 && ty <= C && labyrinth[tx][ty] == '.' && !visited[tx][ty] {
-				visited[tx][ty] = true
+			if tx >= 1 && tx <= R && ty >= 1 && ty <= C && labyrinth[tx][ty] == '.' && distance[tx][ty] == -1 {
 				distance[tx][ty] = distance[x][y] + 1
 				q = append(q, Point{tx, ty})
 			}
